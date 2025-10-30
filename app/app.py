a5470ce308926855b7f76d32a4eaa4626c468d1e
yunnamn <yunnamn@mail.ru>
2025-10-30 17:24:40 +0500

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return render_template("index.html", msg="Hello, CI/CD!")
+
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
